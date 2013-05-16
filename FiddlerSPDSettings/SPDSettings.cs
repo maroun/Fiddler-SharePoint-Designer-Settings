f3752ffcb01f5c737f5054acc41f3cfc791a85ad
Max Nowack <max@unsou.de>
2013-05-16 20:45:59 +0200

    added method to initialize the menu items

--- FiddlerSPDSettings/SPDSettings.cs
+++ FiddlerSPDSettings/SPDSettings.cs
@@ -18,6 +18,55 @@ namespace FiddlerSPDSettings
         private MenuItem miEnableMasterPageEditing;
         private MenuItem mnuSPD;
 
+        private void initializeMenu()
+        {
+            this.miEnableSPD = new System.Windows.Forms.MenuItem("&Enable SharePoint Designer");
+            this.miEnableRevertFromTemplate = new System.Windows.Forms.MenuItem("Enable &Detaching Pages from the Site Definition");
+            this.miEnableMasterPageEditing = new System.Windows.Forms.MenuItem("Enable Customizing &Master Pages and Page Layouts ");
+            this.miEnableUrlStructure = new System.Windows.Forms.MenuItem("Enable Managing of the Web Site URL &Structure ");
+
+            this.miEnableSPD.Index = 0;
+            this.miEnableRevertFromTemplate.Index = 1;
+            this.miEnableMasterPageEditing.Index = 2;
+            this.miEnableUrlStructure.Index = 3;
+
+            this.mnuSPD = new System.Windows.Forms.MenuItem();
+            this.mnuSPD.MenuItems.AddRange(new System.Windows.Forms.MenuItem[] { this.miEnableSPD, this.miEnableRevertFromTemplate, this.miEnableMasterPageEditing, this.miEnableUrlStructure });
+            this.mnuSPD.Text = "SharePoint Designer";
+
+            this.miEnableSPD.Click += new System.EventHandler(this.miEnableSPD_Click);
+            this.miEnableSPD.Checked = enableSpd;
+
+            this.miEnableRevertFromTemplate.Click += new System.EventHandler(this.miEnableRevertFromTemplate_Click);
+            this.miEnableRevertFromTemplate.Checked = enableRevertFromTemplate;
+
+            this.miEnableMasterPageEditing.Click += new System.EventHandler(this.miEnableMasterPageEditing_Click);
+            this.miEnableMasterPageEditing.Checked = enableMasterPageEditing;
+
+            this.miEnableUrlStructure.Click += new System.EventHandler(this.miEnableFileListing_Click);
+            this.miEnableUrlStructure.Checked = enableUrlStructure;
+        }
+
+        public void miEnableFileListing_Click(object sender, EventArgs e)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void miEnableMasterPageEditing_Click(object sender, EventArgs e)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void miEnableRevertFromTemplate_Click(object sender, EventArgs e)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void miEnableSPD_Click(object sender, EventArgs e)
+        {
+            throw new NotImplementedException();
+        }
+
         public void OnPeekAtResponseHeaders(Session oSession)
         {
             throw new NotImplementedException();
@@ -55,7 +104,7 @@ namespace FiddlerSPDSettings
 
         public void OnLoad()
         {
-            throw new NotImplementedException();
+            FiddlerApplication.UI.mnuMain.MenuItems.Add(mnuSPD);
         }
     }
 }
