cae112a50684f6e212295843a269070e01f9da59
Max Nowack <max@unsou.de>
2013-05-16 20:42:51 +0200

    added class for spd settings

---
+++ FiddlerSPDSettings/SPDSettings.cs
@@ -0,0 +1,50 @@
+﻿using Fiddler;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+namespace FiddlerSPDSettings
+{
+    public class SPDSettings : IAutoTamper2
+    {
+        public void OnPeekAtResponseHeaders(Session oSession)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void AutoTamperRequestAfter(Session oSession)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void AutoTamperRequestBefore(Session oSession)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void AutoTamperResponseAfter(Session oSession)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void AutoTamperResponseBefore(Session oSession)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void OnBeforeReturningError(Session oSession)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void OnBeforeUnload()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void OnLoad()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
