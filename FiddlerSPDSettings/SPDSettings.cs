5e35b41db696760a541fbbbb00c676794bb63ed8
Max Nowack <max.nowack@sycor.de>
2014-02-14 11:03:07 +0100

    some refactoring

--- FiddlerSPDSettings/SPDSettings.cs
+++ FiddlerSPDSettings/SPDSettings.cs
@@ -1,49 +1,46 @@
 ﻿using Fiddler;
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
 using System.Windows.Forms;
 namespace FiddlerSPDSettings
 {
     public class SPDSettings : IAutoTamper2
     {
-        private MenuItem miEnableSPD;
-        private MenuItem miEnableUrlStructure;
-        private MenuItem miEnableRevertFromTemplate;
-        private MenuItem miEnableMasterPageEditing;
-        private MenuItem mnuSPD;
+        private MenuItem _miEnableSpd;
+        private MenuItem _miEnableUrlStructure;
+        private MenuItem _miEnableRevertFromTemplate;
+        private MenuItem _miEnableMasterPageEditing;
+        private MenuItem _mnuSpd;
 
-        private void initializeMenu()
+        private void InitializeMenu()
         {
-            this.miEnableSPD = new System.Windows.Forms.MenuItem(Constants.MenuItemSPD);
-            this.miEnableRevertFromTemplate = new System.Windows.Forms.MenuItem(Constants.MenuItemRevertFromTemplate);
-            this.miEnableMasterPageEditing = new System.Windows.Forms.MenuItem(Constants.MenuItemMasterPageEditing);
-            this.miEnableUrlStructure = new System.Windows.Forms.MenuItem(Constants.MenuItemUrlStructure);
+            _miEnableSpd = new MenuItem(Constants.MenuItemSPD);
+            _miEnableRevertFromTemplate = new MenuItem(Constants.MenuItemRevertFromTemplate);
+            _miEnableMasterPageEditing = new MenuItem(Constants.MenuItemMasterPageEditing);
+            _miEnableUrlStructure = new MenuItem(Constants.MenuItemUrlStructure);
 
-            this.miEnableSPD.Index = 0;
-            this.miEnableRevertFromTemplate.Index = 1;
-            this.miEnableMasterPageEditing.Index = 2;
-            this.miEnableUrlStructure.Index = 3;
+            _miEnableSpd.Index = 0;
+            _miEnableRevertFromTemplate.Index = 1;
+            _miEnableMasterPageEditing.Index = 2;
+            _miEnableUrlStructure.Index = 3;
 
-            this.mnuSPD = new System.Windows.Forms.MenuItem();
-            this.mnuSPD.MenuItems.AddRange(new System.Windows.Forms.MenuItem[] { this.miEnableSPD, this.miEnableRevertFromTemplate, this.miEnableMasterPageEditing, this.miEnableUrlStructure });
-            this.mnuSPD.Text = Constants.MenuBarName;
+            _mnuSpd = new MenuItem();
+            _mnuSpd.MenuItems.AddRange(new[] { _miEnableSpd, _miEnableRevertFromTemplate, _miEnableMasterPageEditing, _miEnableUrlStructure });
+            _mnuSpd.Text = Constants.MenuBarName;
 
-            this.miEnableSPD.Click += new System.EventHandler(this.miEnableSPD_Click);
-            this.miEnableSPD.Checked = FiddlerApplication.Prefs.GetBoolPref(string.Format("{0}.{1}", Constants.Prefix, Constants.SPDEnabled), false);
+            _miEnableSpd.Click += miEnableSPD_Click;
+            _miEnableSpd.Checked = FiddlerApplication.Prefs.GetBoolPref(string.Format("{0}.{1}", Constants.Prefix, Constants.SPDEnabled), false);
 
-            this.miEnableRevertFromTemplate.Click += new System.EventHandler(this.miEnableRevertFromTemplate_Click);
-            this.miEnableRevertFromTemplate.Checked = FiddlerApplication.Prefs.GetBoolPref(string.Format("{0}.{1}", Constants.Prefix, Constants.RevertFromTemplateEnabled), false);
+            _miEnableRevertFromTemplate.Click += miEnableRevertFromTemplate_Click;
+            _miEnableRevertFromTemplate.Checked = FiddlerApplication.Prefs.GetBoolPref(string.Format("{0}.{1}", Constants.Prefix, Constants.RevertFromTemplateEnabled), false);
 
-            this.miEnableMasterPageEditing.Click += new System.EventHandler(this.miEnableMasterPageEditing_Click);
-            this.miEnableMasterPageEditing.Checked = FiddlerApplication.Prefs.GetBoolPref(string.Format("{0}.{1}", Constants.Prefix, Constants.MasterPageEditingEnabled), false);
+            _miEnableMasterPageEditing.Click += miEnableMasterPageEditing_Click;
+            _miEnableMasterPageEditing.Checked = FiddlerApplication.Prefs.GetBoolPref(string.Format("{0}.{1}", Constants.Prefix, Constants.MasterPageEditingEnabled), false);
 
-            this.miEnableUrlStructure.Click += new System.EventHandler(this.miEnableUrlStructure_Click);
-            this.miEnableUrlStructure.Checked = FiddlerApplication.Prefs.GetBoolPref(string.Format("{0}.{1}", Constants.Prefix, Constants.UrlStructureEnabled), false);
+            _miEnableUrlStructure.Click += miEnableUrlStructure_Click;
+            _miEnableUrlStructure.Checked = FiddlerApplication.Prefs.GetBoolPref(string.Format("{0}.{1}", Constants.Prefix, Constants.UrlStructureEnabled), false);
         }
 
-        private static void invertCheckboxAndSave(MenuItem mi, string fiddlerSettingName)
+        private static void InvertCheckboxAndSave(MenuItem mi, string fiddlerSettingName)
         {
             mi.Checked = !mi.Checked;
             FiddlerApplication.Prefs.SetBoolPref(fiddlerSettingName, mi.Checked);
@@ -51,27 +48,27 @@ namespace FiddlerSPDSettings
 
         public SPDSettings()
         {
-            this.initializeMenu();
+            InitializeMenu();
         }
 
         public void miEnableUrlStructure_Click(object sender, EventArgs e)
         {
-            invertCheckboxAndSave(this.miEnableUrlStructure, string.Format("{0}.{1}",Constants.Prefix,Constants.UrlStructureEnabled));
+            InvertCheckboxAndSave(_miEnableUrlStructure, string.Format("{0}.{1}",Constants.Prefix,Constants.UrlStructureEnabled));
         }
 
         public void miEnableMasterPageEditing_Click(object sender, EventArgs e)
         {
-            invertCheckboxAndSave(this.miEnableMasterPageEditing, string.Format("{0}.{1}", Constants.Prefix, Constants.MasterPageEditingEnabled));
+            InvertCheckboxAndSave(_miEnableMasterPageEditing, string.Format("{0}.{1}", Constants.Prefix, Constants.MasterPageEditingEnabled));
         }
 
         public void miEnableRevertFromTemplate_Click(object sender, EventArgs e)
         {
-            invertCheckboxAndSave(this.miEnableRevertFromTemplate, string.Format("{0}.{1}", Constants.Prefix, Constants.RevertFromTemplateEnabled));
+            InvertCheckboxAndSave(_miEnableRevertFromTemplate, string.Format("{0}.{1}", Constants.Prefix, Constants.RevertFromTemplateEnabled));
         }
 
         public void miEnableSPD_Click(object sender, EventArgs e)
         {
-            invertCheckboxAndSave(this.miEnableSPD, string.Format("{0}.{1}", Constants.Prefix, Constants.SPDEnabled));
+            InvertCheckboxAndSave(_miEnableSpd, string.Format("{0}.{1}", Constants.Prefix, Constants.SPDEnabled));
         }
 
         public void OnPeekAtResponseHeaders(Session oSession) { }
@@ -84,12 +81,12 @@ namespace FiddlerSPDSettings
 
         public void AutoTamperResponseBefore(Session oSession)
         {
-            if (this.miEnableSPD.Checked || this.miEnableRevertFromTemplate.Checked || this.miEnableMasterPageEditing.Checked || this.miEnableUrlStructure.Checked)
+            if (_miEnableSpd.Checked || _miEnableRevertFromTemplate.Checked || _miEnableMasterPageEditing.Checked || _miEnableUrlStructure.Checked)
             {
                 oSession.utilDecodeResponse();
             }
 
-            if (this.miEnableSPD.Checked)
+            if (_miEnableSpd.Checked)
             {
                 if (oSession.uriContains(Constants.AuthorDll) && oSession.oResponse.headers.ExistsAndContains(Constants.ContentType, Constants.ApplicationRpc))
                 {
@@ -98,7 +95,7 @@ namespace FiddlerSPDSettings
                 }
             }
 
-            if (this.miEnableRevertFromTemplate.Checked)
+            if (_miEnableRevertFromTemplate.Checked)
             {
                 if (oSession.uriContains(Constants.ProcessQuery) && oSession.oResponse.headers.ExistsAndContains(Constants.ContentType, Constants.ApplicationJson))
                 {
@@ -106,7 +103,7 @@ namespace FiddlerSPDSettings
                 }
             }
 
-            if (this.miEnableMasterPageEditing.Checked)
+            if (_miEnableMasterPageEditing.Checked)
             {
                 if (oSession.uriContains(Constants.ProcessQuery) && oSession.oResponse.headers.ExistsAndContains(Constants.ContentType, Constants.ApplicationJson))
                 {
@@ -114,7 +111,7 @@ namespace FiddlerSPDSettings
                 }
             }
 
-            if (this.miEnableUrlStructure.Checked)
+            if (_miEnableUrlStructure.Checked)
             {
                 if (oSession.uriContains(Constants.ProcessQuery) && oSession.oResponse.headers.ExistsAndContains(Constants.ContentType, Constants.ApplicationJson))
                 {
@@ -134,7 +131,7 @@ namespace FiddlerSPDSettings
 
         public void OnLoad()
         {
-            FiddlerApplication.UI.mnuMain.MenuItems.Add(mnuSPD);
+            FiddlerApplication.UI.mnuMain.MenuItems.Add(_mnuSpd);
         }
     }
 }
